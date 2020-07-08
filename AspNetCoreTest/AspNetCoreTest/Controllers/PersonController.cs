2bc6c2494e7ee429840cd93ccb874985ef23f2b8
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-07-08 19:25:16 +0300

    Added person controller

---
+++ AspNetCoreTest/AspNetCoreTest/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var person = new Person();
+            person.Name = "Rolands";
+            person.Surname = "Krumbergs";
+            person.Email = "rolands.krumbergs@outlook.com";
+            person.Age = 28;
+            person.Address = "GDPR";
+            person.IsMale = true;
+
+            return View(person);
+        }
+    }
+}
