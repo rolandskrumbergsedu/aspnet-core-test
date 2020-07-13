84906d98150ecb7c865df261cd91ae712f54242a
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-07-13 22:12:39 +0300

    Added EF tooling

---
+++ AspNetCoreTest/AspNetCoreTest.DataAccess.Tests/PersonRepositoryTests.cs
@@ -0,0 +1,25 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.DataAccess.Tests
+{
+    [TestClass]
+    public class PersonRepositoryTests
+    {
+        [TestMethod]
+        public async Task AddPerson()
+        {
+            var repository = new PersonRepository();
+            await repository.Add(new Person
+            {
+                Id = 1,
+                Name = "Test",
+                Surname = "Testing",
+                Address = "Some address",
+                Age = 10,
+                Email = "test@test.com",
+                IsMale = true
+            });
+        }
+    }
+}
---
+++ AspNetCoreTest/AspNetCoreTest.DataAccess/PersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.DataAccess
+{
+    public class PersonRepository
+    {
+        public async Task Add(Person person)
+        {
+            using (var db = new PersonContext())
+            {
+                db.Add(person);
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
