39a44579bf0dcfc5d2d31fe9dca28908114bbe17
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-07-13 21:58:40 +0300

    Initial context

---
+++ AspNetCoreTest/AspNetCoreTest.DataAccess/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AspNetCoreTest.DataAccess
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public bool IsMale { get; set; }
+    }
+}
---
+++ AspNetCoreTest/AspNetCoreTest.DataAccess/PersonContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCoreTest.DataAccess
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=PersonTest;Integrated Security=True");
+        }
+    }
+}
