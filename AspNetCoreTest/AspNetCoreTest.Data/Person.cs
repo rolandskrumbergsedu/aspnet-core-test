c46329310da7d4bb2afac283fead411fa77dfa99
Rolands Krumbergs <rolands.krumbergs@outlook.com>
2020-07-14 19:21:56 +0300

    Added model and context

--- AspNetCoreTest/AspNetCoreTest.Data/Person.cs
+++ AspNetCoreTest/AspNetCoreTest.Data/Person.cs
@@ -1,7 +1,10 @@
-﻿namespace AspNetCoreTest.Models
+﻿using System;
+
+namespace AspNetCoreTest.Data
 {
     public class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
---
+++ AspNetCoreTest/AspNetCoreTest.Data/PersonDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreTest.Data
+{
+    public class PersonDbContext : DbContext // = Database
+    {
+        public DbSet<Person> People { get; set; } // = Table 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Initial Catalog=PersonTestDb;Integrated Security=True;");
+        }
+    }
+}
